f1151e287fac0ffac57f71ce34dbdaed375091fe
secret coder <introverthacker@proton.me>
2025-10-21 18:56:50 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ import time
 
 st.title("⚡🤖 Real-Time Color Detection Web App")
 st.markdown("**🎨 Detect and track selected colors in real-time using your webcam**  \n👨‍💻 Developed by **Rayyan Ahmed**")
-st.markdown("**Note: On Streamlit Cloud, you cannot capture webcam video. The app will always fail to grab frames.
-To demo on the cloud, you would need to upload a video or image instead of using a webcam or use locally.**")
+st.markdown(" **Note: On Streamlit Cloud, you cannot capture webcam video. The app will always fail to grab frames. To demo on the cloud, you would need to upload a video or image instead of using a webcam or use locally.**")
 
 
 
@@ -192,4 +191,5 @@ while True:
     time.sleep(0.01)
 
 cap.release()
+
 
